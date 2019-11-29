1504e55568fb7958c3600f134cb6b6fd144d5a78
Yousif-Ahmed <yousif.lasheen60@gmail.com>
2019-11-30 01:15:57 +0200

    adding card nine

--- CardNine.cpp
+++ CardNine.cpp
@@ -1,17 +1,42 @@
 #include "CardNine.h"
 
-CardNine::CardNine(const CellPosition& cpos):Card(cpos)
+#include "CardOne.h"
+CardNine::CardNine(const CellPosition& pos) : Card(pos) // set the cell position of the card
+{
+	// setting the inherited cardNumber data member with 9
+	cardNumber = 9; 
+}
+
+CardNine::~CardNine(void)
 {
 }
 
 void CardNine::ReadCardParameters(Grid* pGrid)
 {
+
+	//Getting pointer to the input && output Interfaces from the Grid
+
+	Input* pIn =pGrid->GetInput();
+	Output* pOut=pGrid->GetOutput();
+
+	//Ptinting message to gide the user 
+	// get cell clicked and stored it in a paramewte CellTomove (object from  CellPosition )
+	
+	pOut->PrintMessage("New CardNine: Click at any Cell to move to it");
+	CellTomove=pIn->GetCellClicked();
+	
+
+	//Deletting the status Bar 
+	pOut->ClearStatusBar();
 }
 
 void CardNine::Apply(Grid* pGrid, Player* pPlayer)
 {
-}
+	//Calling Apply() of the base class Card to print the message that I reached this card number
 
-CardNine::~CardNine()
-{
+	Card::Apply(pGrid ,pPlayer);
+
+	//Update player cell with the new clicked position 
+
+	pGrid->UpdatePlayerCell(pPlayer, CellTomove);
 }
