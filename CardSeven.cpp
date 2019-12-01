0334a5e4f4d9d7a032133c81a84278f1e85bbdb7
We'Am <weaam.wewe91@gmail.com>
2019-12-01 15:14:34 +0200

    card seven execution

--- CardSeven.cpp
+++ CardSeven.cpp
@@ -1,7 +1,9 @@
 #include "CardSeven.h"
 
 CardSeven::CardSeven(const CellPosition& cpos):Card(cpos)
-{
+{	
+	// setting the inherited cardNumber data member with 7
+	cardNumber = 7;
 }
 
 void CardSeven::ReadCardParameters(Grid* pGrid)
@@ -10,6 +12,12 @@ void CardSeven::ReadCardParameters(Grid* pGrid)
 
 void CardSeven::Apply(Grid* pGrid, Player* pPlayer)
 {
+	//get a variable that refers to the player's number 
+	//int pnum = pGrid->GetcurrPlayerNumber();
+	for (int i = 0; i < 3; i++)
+	{
+		pGrid->AdvanceCurrentPlayer();
+	}
 }
 
 CardSeven::~CardSeven()
